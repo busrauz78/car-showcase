6b67337ce3f5b4d61db47415cdefb4b72c2f6caf
busrauz78 <busrauz78@gmail.com>
2023-08-05 22:04:56 +0300

    Added filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { Landing } from "@/components";
+import {  Filter, Landing } from "@/components";
+import { BRANDS, FUELS, YEARS } from "@/shared";
 import React from "react";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Landing />
+      <div id="discover" className="home__discover">
+      <div className="home__discover-container">
+        <h2 className="home__discover-title">Car Catalogue</h2>
+        <p>Explore out cars you might like</p>
+      </div>
+      <div className="home__filters">
+        <Filter options={BRANDS} title="Brand"  />
+        <Filter options={FUELS} title="Fuel"  />
+        <Filter options={YEARS} title="Year"  />
+      </div>
+    </div>
     </main>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,19 +3,25 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <header className="header">
-    <nav className="nav">
-      <Link href='/' className='flex justify-center items-center'>
-        <Image
-          src='/assets/logo.svg'
-          alt='logo'
-          width={118}
-          height={18}
-          className='object-contain'
-        />
-      </Link>
-    </nav>
-  </header>
-  )
-}
+    <header>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link href="/" className="flex justify-center items-center">
+            <Image
+              src="/assets/logo.svg"
+              alt="logo"
+              width={118}
+              height={18}
+              className="object-contain"
+            />
+          </Link>
+
+          <Link href="/" className="flex justify-center items-center">
+            <span>Sign in</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 export default Navbar;
